d3c66b36a173fb9f3f4edea501d47c229cb19ce0
LenexTLI <sidneyadb@gmail.com>
2024-11-11 08:37:17 +0400

    JumpPad and Pressure Plate
    Created C# scripts (with actual code inside) for JumpPad.cs and PressurePlate.cs, as well as setting them up in the Scene and testing, all working as intended

--- Assets/Player/Interaction/JumpPad.cs
+++ Assets/Player/Interaction/JumpPad.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float boostForce = 15f;          // Upward force to boost the player
+    public float horizontalBoostFactor = 5f; // Horizontal factor for forward launch
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // Determine the direction of approach based on relative velocity
+                float approachDirection = Mathf.Sign(collision.relativeVelocity.x);
+
+                // Apply an upward and forward horizontal force based on approach direction
+                Vector2 boostVelocity = new Vector2(approachDirection * horizontalBoostFactor, boostForce);
+                rb.velocity = boostVelocity;
+            }
+        }
     }
 }
--- Assets/Player/Interaction/PressurePlate.cs
+++ Assets/Player/Interaction/PressurePlate.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject door;
+    public Vector3 openPosition;
+    public float openSpeed = 3f;
+
+    private Vector3 closedPosition;
+    private bool isActivated = false;
+
+    private void Start()
     {
-        
+        closedPosition = door.transform.position;   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isActivated = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isActivated)
+        {
+            door.transform.position = Vector3.Lerp(door.transform.position, openPosition, Time.deltaTime * openSpeed);
+        }
+        else
+        {
+            door.transform.position = Vector3.Lerp(door.transform.position, closedPosition, Time.deltaTime * openSpeed);
+        }
         
     }
 }
